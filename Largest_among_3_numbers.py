f6ccad7c6be917b8002a0a26b326b9d0c22d7922
Bhavani-S-M <142495873+Bhavani-S-M@users.noreply.github.com>
2025-02-11 18:00:37 +0530

    Update Largest_among_3_numbers.py

--- Largest_among_3_numbers.py
+++ Largest_among_3_numbers.py
@@ -1,12 +1,12 @@
-n1=int(input("Enter 1st number:\n"))
-n2=int(input("Enter 2nd number:\n"))
-n3=int(input("Enter 3rd number:\n"))
+n1 = int(input("Enter 1st number:\n"))
+n2 = int(input("Enter 2nd number:\n"))
+n3 = int(input("Enter 3rd number:\n"))
 
-if(n1>n2 and n1>n3):
-    print("1st number is Greater")
+if n1 > n2 and n1 > n3:
+    print(f"1st number that is {n1} is Greater")
 
-elif(n2>n1 and n2>n3):
-    print("2nd number is Greater")
+elif n2 > n1 and n2 > n3:
+    print(f"2nd number that is {n2} is Greater")
 
 else:
-    print("3rd number is Greater")
+    print(f"3rd number that is {n3} is Greater")
